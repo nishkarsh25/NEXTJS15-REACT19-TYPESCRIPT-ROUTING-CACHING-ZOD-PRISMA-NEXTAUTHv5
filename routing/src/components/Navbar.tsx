468a0fc38a6c9adc4d9488ee899e509b09fbe02a
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-01-31 00:22:18 +0530

    Overall Structure The component returns a navigation bar (nav) wrapped inside a div. The div serves as the container for the navigation bar and has absolute positioning so that it stays fixed relative to its nearest positioned ancestor. The nav element is structured to contain: A home link. A set of navigation links.

--- routing/src/components/Navbar.tsx
+++ routing/src/components/Navbar.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Link from "next/link";
 
-
+const Navbar = () => {
+  return (
+    <div className="absolute z-10 w-full text-3xl text-white p-[20px]">
+      
+    </div>
+  );
+};
 
 
