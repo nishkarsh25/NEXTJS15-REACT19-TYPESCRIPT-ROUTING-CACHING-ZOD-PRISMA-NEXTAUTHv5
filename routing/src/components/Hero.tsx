f974fd313698558153afabe375f20dc87e28164d
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-01-31 09:39:16 +0530

    Component Structure The component consists of:
    A full-screen background image (div with relative h-screen).
    A dark gradient overlay (bg-gradient-to-r from-slate-900).
    A text overlay displaying "Professional Gym Trainer".

--- routing/src/components/Hero.tsx
+++ routing/src/components/Hero.tsx
@@ -2,6 +2,12 @@ import Image from "next/image";
 import React from "react";
 import GymMan from "public/gyman.jpg";
 
-
+const Hero = () => {
+  return (
+    <div className="relative h-screen">
+      
+    </div>
+  );
+};
 
 
