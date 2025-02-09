003c088791114fbe14ad478143318847be2e6039
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-02-09 23:09:35 +0530

    3. Defining the Home Page Component

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -6,6 +6,8 @@ import React from "react";
 // export const dynamic = "force-dynamic";//disabling the caching making it -> dynamic route or use the below method
 // export const revalidate =0; //now u can npm run build to see it back a dynamic route hance ther will be no caching always fresh data will be available
 
-
+const Home = async () => {
+  
+};
 
 
