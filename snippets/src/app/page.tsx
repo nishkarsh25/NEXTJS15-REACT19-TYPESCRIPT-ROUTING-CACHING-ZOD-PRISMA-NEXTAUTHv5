1fa1b875f56b6c113ae3889831b3257c30f7e1c4
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-02-09 23:08:57 +0530

    2. Explanation of Caching and Dynamic Behavior By default, Next.js caches data fetched inside a server component. However, this behavior can be changed using dynamic settings.
    A. Disabling Caching Using Dynamic Mode (Commented Out)
    A configuration is provided to force the route to be dynamic, ensuring fresh data is always fetched.
    This is done by disabling automatic caching, making every request fetch fresh data from the database.
    B. Alternative Method: Disabling Revalidation
    Another approach is explicitly setting the revalidation time to zero.
    This means that when the site is built, the data will not be cached and will always be dynamically fetched.

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -3,7 +3,8 @@ import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 import React from "react";
 
-
+// export const dynamic = "force-dynamic";//disabling the caching making it -> dynamic route or use the below method
+// export const revalidate =0; //now u can npm run build to see it back a dynamic route hance ther will be no caching always fresh data will be available
 
 
 
