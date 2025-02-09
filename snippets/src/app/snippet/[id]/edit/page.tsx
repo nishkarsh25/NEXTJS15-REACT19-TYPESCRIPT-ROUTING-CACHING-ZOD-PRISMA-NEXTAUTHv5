f9f73d78eb91a798af58172ea6bebda98a70e72e
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-02-09 23:19:04 +0530

    2. Defining the Component and Props The component receives params as a prop, which contains the route parameters. The params prop is an asynchronous promise that contains the id of the snippet that the user wants to edit. The id is used to retrieve the correct snippet from the database.

--- snippets/src/app/snippet/[id]/edit/page.tsx
+++ snippets/src/app/snippet/[id]/edit/page.tsx
@@ -3,5 +3,20 @@ import { prisma } from "@/lib/prisma";
 import { notFound } from "next/navigation";
 import React from "react";
 
+const EditPageSnippet = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const id = Number((await params).id);
+  const snippet = await prisma.snippet.findUnique({
+    where: {
+      id,
+    },
+  });
 
+  if (!snippet) notFound();
+
+  
+};
 
