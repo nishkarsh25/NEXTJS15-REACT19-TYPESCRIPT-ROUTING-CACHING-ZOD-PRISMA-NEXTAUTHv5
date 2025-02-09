1880f35e3b8a405fc25d7b67dbced788f3e5f63b
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-02-09 22:30:53 +0530

    5. Defining the Create Function This function is responsible for creating a new snippet based on user input.
    A. Handling Input Data
    The function accepts two parameters:
    The previous state (used for error handling).
    A FormData object that contains user input from a form submission.
    B. Extracting Form Data
    The form input fields for title and code are extracted.
    C. Validating User Input
    The title must be at least 4 characters long.
    The code must be at least 8 characters long.
    If validation fails, an error message is returned.
    D. Creating the New Snippet in the Database
    If validation passes, a new record is inserted into the database using Prisma.
    The title and code fields are stored.
    E. Revalidating the Cache
    The homepage cache is refreshed to display the new snippet.
    F. Handling Errors
    If any error occurs during the process, it is caught and returned as an error message.
    G. Redirecting the User
    After successfully creating the snippet, the user is redirected to the homepage.

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -29,4 +29,37 @@ export const deleteSnippet = async (id: number) => {
   redirect("/");
 };
 
+export const createSnippet = async (
+  prevState: { message: string },
+  formData: FormData
+) => {
+  try {
+    const title = formData.get("title");
+    const code = formData.get("code");
 
+    if (typeof title != "string" || title.length < 4) {
+      return { message: "Title is required and must be longer" };
+    }
+
+    if (typeof code != "string" || code.length < 8) {
+      return { message: "Code is required and must be longer" };
+    }
+
+    const snippet = await prisma.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    revalidatePath("/");
+    console.log("Snippet created", snippet);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return { message: error.message };
+    }
+
+    return { message: "Some internal server error" };
+  }
+  redirect("/");
+};
