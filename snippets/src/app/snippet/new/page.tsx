a4549b212b116788957b9e4e1fed356469c46d3a
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-02-09 23:11:59 +0530

    2. Importing Dependencies Several modules and components are imported to provide essential functionality:
    A. Server Action (createSnippet)
    A function for handling form submissions is imported from an actions file.
    This function is a server action, meaning it runs on the server when the form is submitted.
    B. UI Components (Button, Input, Label, Textarea)
    Reusable UI components are imported from a component library.
    These components help create a structured and styled form interface.
    C. React and useActionState Hook
    React is imported because this file defines a React component.
    A built-in React hook is used to manage form state and handle server-side form submissions asynchronously.

--- snippets/src/app/snippet/new/page.tsx
+++ snippets/src/app/snippet/new/page.tsx
@@ -1,5 +1,10 @@
 "use client"
-
+import { createSnippet } from "@/actions";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import React, { useActionState } from "react";
 
 
 
