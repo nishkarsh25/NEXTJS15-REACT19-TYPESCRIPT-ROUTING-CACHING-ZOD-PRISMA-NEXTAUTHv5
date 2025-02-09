3a2cedfd2ab49d50737e97a668c13e9109ecbeac
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-02-09 23:16:09 +0530

    5. Structuring the Component UI A. Displaying the Snippet Title The snippet’s title is displayed using a bold, large font. The title is enclosed in a flex container for better alignment. B. Providing Edit and Delete Options An edit button is placed inside a Next.js Link component, allowing users to navigate to the snippet’s edit page. The delete button is wrapped in a <form> to submit the delete request securely to the server. C. Displaying the Snippet Code The code content is displayed inside a preformatted text block (<pre> and <code>). A gray background is applied for readability, mimicking a code editor style.

--- snippets/src/app/snippet/[id]/page.tsx
+++ snippets/src/app/snippet/[id]/page.tsx
@@ -26,7 +26,25 @@ const SnippetDetailPage: React.FC<SnippetDetailsProps> = async ({ params }) => {
 
   if (!snippet) notFound();
   return (
-   
+    <div className="flex flex-col gap-5">
+      <div className="flex items-center justify-between">
+        <h1 className="font-bold text-xl">{snippet?.title}</h1>
+        <div className="flex items-center gap-2">
+          <Link href={`/snippet/${snippet.id}/edit`}>
+            {" "}
+            <Button>Edit</Button>
+          </Link>
+          <form action={deleteSnippetAction}>
+            <Button variant={"destructive"} type="submit">
+              Delete
+            </Button>
+          </form>
+        </div>
+      </div>
+      <pre className="p-3 bg-gray-200 rounded border-gray-200">
+        <code>{snippet?.code}</code>
+      </pre>
+    </div>
   );
 };
 
