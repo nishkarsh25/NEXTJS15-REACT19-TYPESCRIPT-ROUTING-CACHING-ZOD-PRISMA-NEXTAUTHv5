9bd831d6c83b9d50bd440f03225c47593ad25e2d
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-02-09 23:12:30 +0530

    4. Structuring the Form A. Handling Form Submission The form is configured to automatically call the server action when submitted. The form data is collected and sent to the backend without requiring JavaScript event handlers. B. Creating Input Fields A label and input field are created for entering the snippet title. A label and textarea are used for entering the snippet code. C. Displaying Error Messages If the server returns an error message (e.g., invalid title or code), it is displayed in a styled alert box. D. Submitting the Form A button is used to submit the form. It ensures a structured UI and a consistent user experience across the app.

--- snippets/src/app/snippet/new/page.tsx
+++ snippets/src/app/snippet/new/page.tsx
@@ -10,7 +10,22 @@ const CreateSnippetPage = () => {
   
   const [formStateData,action] = useActionState(createSnippet,{message:""}) 
 
-  
+  return (
+    <form action={action}>
+      <div>
+        <Label>Title</Label>
+        <Input type="text" name="title" id="title"></Input>
+      </div>
+      <div className="mb-4">
+        <Label>Code</Label>
+        <Textarea name="code" id="code"></Textarea>
+      </div>
+      {formStateData.message && <div className="p-2 bg-red-300 border-2 border-red-600">{formStateData.message}</div>}
+      <Button type="submit" className="my-4">
+        New
+      </Button>
+    </form>
+  );
 };
 
 
