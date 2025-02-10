576a9d731b95a75244d52a497907e9b232152209
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-02-10 15:12:41 +0530

    2. Importing Dependencies A. Monaco Editor The Monaco Editor is imported from the @monaco-editor/react package. Monaco is the code editor used in Visual Studio Code and provides a powerful environment for editing code with features like syntax highlighting and auto-completion. B. React and useState Hook React is imported because this file defines a React functional component. The useState hook is used to manage the state of the code being edited. C. Snippet Type from Prisma The Snippet type is imported from Prisma's generated client to define the shape of the data passed to this component. D. Button Component A button component is imported, which will be used for the Save button in the form. E. Save Snippet Action The saveSnippet function is imported from the actions file. This function is used to save the edited snippet to the database.

--- snippets/src/components/EditSnippetForm.tsx
+++ snippets/src/components/EditSnippetForm.tsx
@@ -1,5 +1,9 @@
 "use client";
-
+import { Editor } from "@monaco-editor/react";
+import React, { useState } from "react";
+import type { Snippet } from "@prisma/client";
+import { Button } from "./ui/button";
+import { saveSnippet } from "@/actions";
 
 
 
