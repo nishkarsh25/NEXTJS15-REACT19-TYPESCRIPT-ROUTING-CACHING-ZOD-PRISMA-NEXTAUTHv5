6bcb5a6ffc93c7e90a6f33fdf0945cfe8d2b90e9
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-02-11 19:15:10 +0530

    2. Checking for Missing Environment Variables The code checks if the GitHub client ID and GitHub client secret are available in the environment variables (process.env.GITHUB_CLIENT_ID and process.env.GITHUB_CLIENT_SECRET). If either of these values is missing, an error is thrown to alert the developer that authentication cannot proceed without these values.

--- discuss/src/auth.ts
+++ discuss/src/auth.ts
@@ -3,5 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import GitHubProvider from "next-auth/providers/github";
 import { prisma } from "@/lib";
 
-
+if (!process.env.GITHUB_CLIENT_ID || !process.env.GITHUB_CLIENT_SECRET) {
+  throw new Error("Missing github client id or client secret");
+}
 
