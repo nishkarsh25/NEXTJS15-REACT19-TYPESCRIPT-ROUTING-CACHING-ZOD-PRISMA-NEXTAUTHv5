55252947f8ec0ba4dc17eb72ac78c607e21c6f28
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-02-11 22:51:33 +0530

    3. Defining createTopicSchema (Validation Schema) Uses Zod to validate form data before processing. Rules for Validation: name: Must be a string with a minimum of 3 characters. Can contain only lowercase letters and hyphens (no spaces or uppercase letters). description: Must be a string with at least 10 characters.

--- discuss/src/actions/create-topics.ts
+++ discuss/src/actions/create-topics.ts
@@ -6,7 +6,13 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 
-
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, { message: "Must be lowercase letter without spaces" }),
+  description: z.string().min(10),
+});
 
 
 
