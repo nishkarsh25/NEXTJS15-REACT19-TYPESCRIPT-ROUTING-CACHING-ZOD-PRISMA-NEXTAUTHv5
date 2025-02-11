cff08f2b2dd0eb4da35642981c52c70ac07ff11b
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-02-11 22:59:13 +0530

    5. createPost Function (Main Logic) A. Validating Form Data Extracts title and content from formData. Uses safeParse() to check if they match the validation schema. If validation fails, returns specific error messages. B. Checking Authentication Calls auth() to get the user's session. If the user is not logged in, returns an error "You have to login first". C. Finding the Topic Searches for the topic using the provided slug. If the topic doesn’t exist, returns an error "Topic not found". D. Storing Post Data in the Database Attempts to create a new post in the Post table using Prisma.
    Stores:
    
    title: Title of the post.
    content: Main content of the post.
    userId: The ID of the authenticated user (ensuring the post is linked to them).
    topicId: The ID of the topic where the post is created.
    Error Handling:
    
    If an error occurs during insertion, returns a formError message.
    E. Clearing Cache and Redirecting
    Calls revalidatePath("/topics/{slug}") to clear the cache for the topic page, ensuring the new post appears immediately.
    Uses redirect("/topics/{slug}/posts/{post.id}") to send the user to the new post's page.

--- discuss/src/actions/create-post.ts
+++ discuss/src/actions/create-post.ts
@@ -19,4 +19,74 @@ type CreatePostFormState = {
     }
 }
 
+export const createPost = async (slug:string,prevState:CreatePostFormState,formData: FormData) : Promise<CreatePostFormState> => {
+  
+  const result = createPostSchema.safeParse({
+    title:formData.get('title'),
+    content:formData.get('content')
+  })
 
+  if(!result.success)
+  {
+     return {
+         errors:result.error.flatten().fieldErrors
+     }
+  }
+
+  const session = await auth();
+
+  if(!session || !session.user || !session.user.id)
+  {
+    return {
+        errors:{
+            formError:["You have to login first"]
+        }
+    }
+  }
+
+  const topic = await prisma.topic.findFirst({
+    where:{slug}
+  })
+
+  if(!topic)
+  {
+    return {
+        errors:{
+            formError:['Topic not found']
+        }
+    }
+  }
+
+  let post:Post;
+
+  try {
+    post = await  prisma.post.create({
+        data:{
+            title:result.data.title,
+            content:result.data.content,
+            userId:session.user.id,
+            topicId:topic.id
+        }
+    })
+
+  } catch (error:unknown) {
+    if(error instanceof Error)
+    {
+        return  {
+            errors:{
+                formError:[error.message]
+            }
+        }
+    }
+
+    else{
+        return {
+            errors:{
+                formError:['Failed to create post']
+            }
+        }
+    }
+  }
+  revalidatePath(`/topics/${slug}`)
+  redirect(`/topics/${slug}/posts/${post.id}`);
+};
