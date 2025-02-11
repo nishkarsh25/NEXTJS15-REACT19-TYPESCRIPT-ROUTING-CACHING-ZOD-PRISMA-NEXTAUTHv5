764d8e3f0b311a766cf9bc4b40b28a7d638a995e
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-02-11 22:51:23 +0530

    2. Importing Dependencies auth: Handles user authentication. prisma: Prisma Client for interacting with the database. Topic: The Prisma model that represents a topic. revalidatePath: Clears the cache for a specific path after a database update. redirect: Redirects the user to a new page after topic creation. zod: A validation library used to enforce data integrity.

--- discuss/src/actions/create-topics.ts
+++ discuss/src/actions/create-topics.ts
@@ -1,5 +1,10 @@
 "use server";
-
+import { auth } from "@/auth";
+import { prisma } from "@/lib";
+import { Topic } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
 
 
 
