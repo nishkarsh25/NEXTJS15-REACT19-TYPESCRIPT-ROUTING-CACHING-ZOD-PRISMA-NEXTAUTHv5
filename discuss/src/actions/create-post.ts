0ac128cc16b5356c284556d186a81ce49ef1c57c
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-02-11 22:58:50 +0530

    3. Defining createPostSchema (Validation Schema) Uses Zod to validate form inputs. Ensures: title: Must be a string with at least 3 characters. content: Must be a string with at least 10 characters.

--- discuss/src/actions/create-post.ts
+++ discuss/src/actions/create-post.ts
@@ -6,7 +6,10 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 
-
+const createPostSchema = z.object({
+    title:z.string().min(3),
+    content:z.string().min(10),
+})
 
 
 
