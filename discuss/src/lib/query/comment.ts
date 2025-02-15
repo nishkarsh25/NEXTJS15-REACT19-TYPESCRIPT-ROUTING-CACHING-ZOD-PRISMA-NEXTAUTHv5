bd45fe93c3bbddd72e608d914e702461deae11a2
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-02-15 19:17:43 +0530

    2. Type Definition for Comments with Author Data The code defines a custom TypeScript type called CommentWithAuthor:
    This type extends the standard Prisma Comment type.
    It adds an embedded user object, which includes:
    name: The author’s name (can be a string or null if not provided).
    image: The author’s profile image (can be a string or null if not provided).
    This type ensures that every fetched comment will include author details.

--- discuss/src/lib/query/comment.ts
+++ discuss/src/lib/query/comment.ts
@@ -2,6 +2,8 @@ import type { Comment } from "@prisma/client";
 import { prisma } from "..";
 import { cache } from "react";
 
-
+type CommentWithAuthor = Comment & {
+  user: { name: string | null; image: string | null };
+};
 
 
