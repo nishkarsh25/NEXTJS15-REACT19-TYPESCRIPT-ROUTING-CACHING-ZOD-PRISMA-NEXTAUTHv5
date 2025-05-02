1a70a31158adf8fbe699b8844df244e2787d14b2
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-02 15:33:06 +0530

    Server Component The async keyword on the function component shows that it's a server-side rendered component.
    This means the data fetching (fetchCommentByPostId) is done on the server before the page is sent to the client, improving performance and SEO.

--- discuss/src/components/comments/comment-list.tsx
+++ discuss/src/components/comments/comment-list.tsx
@@ -6,5 +6,14 @@ type CoommentListProps = {
   postId: string;
 };
 
+const CommentList: React.FC<CoommentListProps> = async ({ postId }) => {
+  const comments = await fetchCommentByPostId(postId);
+
+  const topLevelComments = comments.filter(
+    (comment) => comment.parentId == null
+  );
+
+  
+};
 
 
