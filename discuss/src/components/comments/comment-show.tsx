2b2554929226990a1a4d108b9936937c0d1f58df
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-02 15:34:53 +0530

    Imports fetchCommentByPostId: Fetches all comments related to a specific post from the backend.
    React: For using React features.
    
    Avatar, AvatarImage: UI components used to render the commenter's profile image.
    
    CommentCreateForm: A form allowing users to reply to a comment.

---
+++ discuss/src/components/comments/comment-show.tsx
@@ -0,0 +1,10 @@
+import { fetchCommentByPostId } from "@/lib/query/comment";
+import React from "react";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import CommentCreateForm from "./comment-create-form";
+
+
+
+
+
+
