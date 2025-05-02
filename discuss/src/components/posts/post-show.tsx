9c319ede67c8a30e1ad000335e7d238dc394a249
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-02 15:48:01 +0530

    Fetching Post Data:
    The PostShow component is an asynchronous function, indicated by the async keyword.
    
    Inside the component, it uses await new Promise((res) => setTimeout(res, 3000)); to introduce a 3-second delay before fetching the post. This is likely used for simulating loading or introducing a delay for testing purposes.
    
    Fetching the Post:
    
    It then fetches the post from the database using prisma.post.findFirst(). The findFirst method is used to retrieve the first post that matches the given criteria, in this case, a post with the ID equal to postId.
    
    If no post is found with that ID, it calls notFound(), which triggers a 404 error response, rendering a "not found" page.

--- discuss/src/components/posts/post-show.tsx
+++ discuss/src/components/posts/post-show.tsx
@@ -7,6 +7,17 @@ type PostShowProps = {
   postId: string;
 };
 
+const PostShow: React.FC<PostShowProps> = async ({ postId }) => {
+  await new Promise((res) => setTimeout(res,3000));
+  const post = await prisma.post.findFirst({
+    where: {
+      id: postId,
+    },
+  });
 
+  if (!post) notFound();
+
+ 
+};
 
 
