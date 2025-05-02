524964b640bf28a69845307476bd7a5ceddaa972
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-02 15:43:25 +0530

    mports:
    UI Components:
    
    Button, Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger, DialogFooter: These components are used to create the modal dialog window, making it easy for users to create a new post.
    
    Input, Label, and Textarea: These are the form fields for entering the title and content of the post.
    
    useActionState: This hook is used to manage the state of the form, including handling the action (post creation) and storing any errors that might occur during submission.
    
    createPost: This is the action function that contains the logic for creating a new post. It is triggered when the form is submitted.

---
+++ discuss/src/components/posts/PostCreateForm.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "../ui/textarea";
+import { useActionState } from "react";
+import { createPost } from "@/actions/create-post";
+
+
+
+
+
+
