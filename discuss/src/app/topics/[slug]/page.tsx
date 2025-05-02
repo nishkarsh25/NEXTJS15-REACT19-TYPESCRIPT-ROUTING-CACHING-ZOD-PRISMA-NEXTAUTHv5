92c2ce3c3877822689651829a022141ccffd6c46
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-02 15:24:09 +0530

    Component Props TopicShowPageProps: The component expects a prop named params, which is a Promise containing an object with a slug property. The slug represents the unique identifier for a specific topic.
    This slug is used to filter posts related to that specific topic.

--- discuss/src/app/topics/[slug]/page.tsx
+++ discuss/src/app/topics/[slug]/page.tsx
@@ -3,7 +3,9 @@ import PostCreateForm from "@/components/posts/PostCreateForm";
 import { fetchPostbyTopicSlug } from "@/lib/query/post";
 import React from "react";
 
-
+type TopicShowPageProps = {
+  params: Promise<{ slug: string }>;
+};
 
 
 
