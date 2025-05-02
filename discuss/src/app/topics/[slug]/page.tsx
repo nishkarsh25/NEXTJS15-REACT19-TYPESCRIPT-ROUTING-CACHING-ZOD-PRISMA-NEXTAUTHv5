145c5a9e841255a070c476a46387fb0e1c6d816e
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-02 15:23:58 +0530

    Imports PostList: A component that renders a list of posts. It takes a function (fetchData) as a prop that fetches posts based on certain criteria, such as by topic slug.
    PostCreateForm: A form component for creating a new post. It accepts a slug prop, which is the identifier for the topic under which the new post will be created.
    
    fetchPostbyTopicSlug: A function imported from @/lib/query/post. This function fetches posts related to a specific topic, identified by the topic’s slug.
    
    React: The core React library for building user interfaces. It’s used to define the component structure and manage the component lifecycle.

---
+++ discuss/src/app/topics/[slug]/page.tsx
@@ -0,0 +1,10 @@
+import PostList from "@/components/posts/post-list";
+import PostCreateForm from "@/components/posts/PostCreateForm";
+import { fetchPostbyTopicSlug } from "@/lib/query/post";
+import React from "react";
+
+
+
+
+
+
