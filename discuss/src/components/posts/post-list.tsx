06ca41d4da1cc48a83ddf36fa3b0f769b327fef1
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-02 15:09:21 +0530

    Component Props and Typing The component accepts one prop: fetchData, which is an async function that returns a list of posts conforming to the PostWithData type.
    This design pattern decouples the data-fetching logic from the component UI, enabling reuse across different views with different data sources.

--- discuss/src/components/posts/post-list.tsx
+++ discuss/src/components/posts/post-list.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
 import { PostWithData } from "@/lib/query/post";
 
-
+type PostListProps = {
+  fetchData: () => Promise<PostWithData[]>;
+};
 
 
 
