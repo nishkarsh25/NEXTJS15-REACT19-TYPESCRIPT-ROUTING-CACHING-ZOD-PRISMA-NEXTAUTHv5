6c9b0fbd749347f4e71a5a3a67b4a1956f4d0181
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-02 15:07:57 +0530

    Input Field Configuration The input is of type text, and is styled using a utility class for rounded corners and centered content.
    The defaultValue is set to the current search term from the URL (searchParams.get("term")), or an empty string if it's not present.
    
    The name="term" ensures that this value is passed correctly to the server action as a form field when the form is submitted.

--- discuss/src/components/search-input.tsx
+++ discuss/src/components/search-input.tsx
@@ -8,7 +8,13 @@ const SearchInput = () => {
   const searchParams = useSearchParams();
   return (
     <form action={search}>
-      
+      <Input
+        defaultValue={searchParams.get("term") || ""}
+        className="rounded flex justify-center"
+        type="text"
+        placeholder="Search post..."
+        name="term"
+      />
     </form>
   );
 };
