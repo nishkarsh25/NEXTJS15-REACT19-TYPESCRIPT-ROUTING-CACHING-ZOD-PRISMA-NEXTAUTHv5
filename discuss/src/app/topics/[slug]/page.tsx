ef0b946e8dda44eadb28364a16b7dda9c3d14043
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-02 15:24:20 +0530

    Async Function and Data Fetching const slug = (await params).slug;:
    This line extracts the slug from the params prop, which is a promise. The await keyword ensures that the component waits for the promise to resolve before continuing.
    
    The slug is then used to filter and fetch posts related to that specific topic.

--- discuss/src/app/topics/[slug]/page.tsx
+++ discuss/src/app/topics/[slug]/page.tsx
@@ -7,6 +7,9 @@ type TopicShowPageProps = {
   params: Promise<{ slug: string }>;
 };
 
-
+const TopicShowPage: React.FC<TopicShowPageProps> = async ({ params }) => {
+  const slug = (await params).slug;
+  
+};
 
 
