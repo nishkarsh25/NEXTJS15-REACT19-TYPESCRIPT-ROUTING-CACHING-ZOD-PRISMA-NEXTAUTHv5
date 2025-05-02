878e6fad8350197dc12362c050f82f35f7b3adbe
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-02 14:42:05 +0530

    Component Structure and Imports PostList: A component that is responsible for displaying a list of posts. It receives a data-fetching function (fetchData) as a prop.
    TopicCreateForm: A form component that allows users to create new topics. It likely includes inputs for title, content, and possibly tags or categories.
    
    fetchTopPosts: A function imported from a utility library responsible for fetching top posts. This function is passed to PostList to supply the post data.

---
+++ discuss/src/app/page.tsx
@@ -0,0 +1,8 @@
+import PostList from "@/components/posts/post-list";
+import TopicCreateForm from "@/components/topics/TopicCreateForm";
+import { fetchTopPosts } from "@/lib/query/post";
+import React from "react";
+
+
+
+
