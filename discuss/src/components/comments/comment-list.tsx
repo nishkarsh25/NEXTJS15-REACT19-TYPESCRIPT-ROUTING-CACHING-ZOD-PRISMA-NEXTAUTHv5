9b2e356f085eca4e1a3df800aeb867f1915fe727
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-02 15:33:36 +0530

    Data Fetching comments = await fetchCommentByPostId(postId):
    This fetches all comments associated with the given postId, including both top-level comments and replies.

--- discuss/src/components/comments/comment-list.tsx
+++ discuss/src/components/comments/comment-list.tsx
@@ -9,10 +9,6 @@ type CoommentListProps = {
 const CommentList: React.FC<CoommentListProps> = async ({ postId }) => {
   const comments = await fetchCommentByPostId(postId);
 
-  const topLevelComments = comments.filter(
-    (comment) => comment.parentId == null
-  );
-
   
 };
 
