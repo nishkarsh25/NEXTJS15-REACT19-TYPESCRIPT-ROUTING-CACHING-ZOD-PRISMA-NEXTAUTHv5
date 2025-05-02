3e5468db7f1f4c749405fe83d1880b5d3a12563f
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-02 14:48:39 +0530

    Metadata Definition A metadata object is declared, which includes a title and description for the application.
    This metadata is used by Next.js to populate the HTML <head>, aiding in SEO (Search Engine Optimization) and browser tab naming.
    
    It describes the app as a Next.js-generated application.

---
+++ discuss/src/app/layout.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { SessionProvider } from "next-auth/react";
+import HeaderPage from "@/components/header";
+
+
+
+
+
