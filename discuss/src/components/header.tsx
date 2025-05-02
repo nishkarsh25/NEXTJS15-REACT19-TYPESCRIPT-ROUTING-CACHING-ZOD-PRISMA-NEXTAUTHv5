646c4ab1f1e275174bee7be7b41f487b2cf1697b
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-02 14:57:51 +0530

    Layout and Styling The component uses Tailwind CSS and applies a 3-column grid layout to the header:
    Left: App title ("Discuss")
    
    Center: Search bar
    
    Right: Authentication section (login/signup/profile)
    
    Spacing and alignment are handled with utility classes like flex, justify-start, justify-end, gap-2, and margin utilities (mt-2).

--- discuss/src/components/header.tsx
+++ discuss/src/components/header.tsx
@@ -2,6 +2,11 @@ import React, { Suspense } from "react";
 import AuthHeader from "./auth-header";
 import SearchInput from "./search-input";
 
-
+const HeaderPage = async () => {
+  // const session = await auth(); session.user accessing session in a server component auth from @/auth it stores cookies so it will make any page using this header as dynnamic like home page and all other pages
+  // so make a client component within this component handling session else all pages with header will become dynamic and no caching
+  // we try to keep as much page as static pages as possible so that caching can be implemented
+  
+};
 
 
