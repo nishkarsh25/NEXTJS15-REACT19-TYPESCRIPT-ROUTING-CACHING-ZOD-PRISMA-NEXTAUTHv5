0cfe014f71db7266d5135073b404cbac14aef917
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-02 15:32:55 +0530

    Props postId: Passed as a prop; this is the unique identifier of the post whose comments are being listed.

--- discuss/src/components/comments/comment-list.tsx
+++ discuss/src/components/comments/comment-list.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import CommentShow from "./comment-show";
 import { fetchCommentByPostId } from "@/lib/query/comment";
 
-
+type CoommentListProps = {
+  postId: string;
+};
 
 
 
