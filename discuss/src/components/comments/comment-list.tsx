23b7bbae7f003bd64aeae941576e199347a584eb
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-02 15:33:44 +0530

    Filtering Top-Level Comments topLevelComments = comments.filter(comment => comment.parentId == null):
    Filters out replies, keeping only the top-level comments (those directly posted on the post, not as replies to other comments).

--- discuss/src/components/comments/comment-list.tsx
+++ discuss/src/components/comments/comment-list.tsx
@@ -9,7 +9,15 @@ type CoommentListProps = {
 const CommentList: React.FC<CoommentListProps> = async ({ postId }) => {
   const comments = await fetchCommentByPostId(postId);
 
-  
+  const topLevelComments = comments.filter(
+    (comment) => comment.parentId == null
+  );
+
+  return (
+    <div>
+      
+    </div>
+  );
 };
 
 
